6a372a0a23b7e63530520c558750e9f521ebda6d
Igor Tyapkin <igor.tyapkin1996@yandex.ru>
2025-12-17 12:11:13 +0600

    fix: fixed service ports

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.AUTH_PORT ? Number(process.env.AUTH_PORT) : 4001;
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
+app.get('/some', (req, res) => {
+  res.send({ message: 'Hello API' });
+});
+
 app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`[ ready ] Auth Service: http://${host}:${port}`);
 });
